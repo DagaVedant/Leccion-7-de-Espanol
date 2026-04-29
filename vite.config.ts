55bd07d07adc980334feb18319862fece4b7f442
Vedant Daga <vdsupercool@gmail.com>
2026-04-29 11:27:18 -0400

    Use singlefile plugin for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Leccion-7-de-Espanol/', // must match your repo name exactly
+  plugins: [react(), viteSingleFile()],
+  base: './',
 })
